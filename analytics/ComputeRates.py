b2d8eb8ff19b5a4f6e842d42b134a32f1edb5afc
Steve Herrin <steve.herrin@gmail.com>
2013-09-09 17:00:06 -0700

    Finish separating out the db code and changing the schcema some to be more general.

---
+++ analytics/ComputeRates.py
@@ -0,0 +1,56 @@
+from __future__ import division
+import psycopg2
+import numpy as np
+import pylab as P
+from db_info import conn_string
+from scipy.stats import linregress
+from math import log
+
+conn = psycopg2.connect(conn_string)
+
+outer_cur = conn.cursor()
+inner_cur = conn.cursor()
+
+max_d = 200 # meters
+
+outer_cur.execute("TRUNCATE TABLE rates")
+
+# Probably could stack subquerires inside of subqueries
+# instead of looping, but this logic is clearer to me
+outer_cur.execute("SELECT id, vehicle FROM parking ORDER BY id")
+for parking_spot in outer_cur:
+    id = int(parking_spot[0])
+
+    # Normalize by the density of nearby spots
+    inner_cur.execute("""SELECT SUM(n_spots)
+                            FROM parking AS park1,
+                            (SELECT location, vehicle FROM parking
+                             WHERE id=%s) AS park2
+                            WHERE ST_DWithin(park1.location, park2.location, %s)
+                            AND park1.vehicle = park2.vehicle
+                            AND (park1.year_installed IS NULL
+                                 OR park1.year_installed != 2013)""",
+                            (id, max_d))
+    # Edge case where a lone rack was installed in 2013
+    try:
+        n_spots = int(inner_cur.fetchone()[0])
+    except TypeError:
+        rate = None
+    else:
+        inner_cur.execute("""SELECT COUNT(*)
+                                FROM crimes,
+                                (SELECT location, vehicle FROM parking WHERE id=%s) AS park
+                                WHERE crimes.vehicle=park.vehicle
+                                AND EXTRACT(YEAR FROM t) < 2013
+                                AND ST_DWithin(park.location, crimes.location, %s)""",
+                            (id, max_d))
+        n_crimes = int(inner_cur.fetchone()[0])
+
+        rate = n_crimes/n_spots
+
+    inner_cur.execute("""INSERT INTO rates (id, rate)
+                       VALUES (%s, %s)""", (id, rate))
+
+    conn.commit()
+
+conn.close()
--- web/web.py
+++ web/web.py
@@ -11,7 +11,8 @@ from flask import session
 from flask import url_for
 from operator import itemgetter
 
-import psycopg2
+import pprint
+import db_interface
 import re
 import my_db_config
 
@@ -30,34 +31,21 @@ app.config.from_object(__name__)
 app.debug = True
 
 def connect_db():
-  """ Returns a connection to the database
-      made using setings in app.config
-  """
-  return psycopg2.connect(host = app.config['DB_HOST'],
-                          dbname = app.config['DB_NAME'],
-                          user = app.config['DB_USER'],
-                          password = app.config['DB_PASSWORD'])
+    """ Returns an interface to the database
+        made using setings in app.config
+    """
+    return db_interface.db_interface(host = app.config['DB_HOST'],
+                                     dbname = app.config['DB_NAME'],
+                                     user = app.config['DB_USER'],
+                                     password = app.config['DB_PASSWORD'])
 
 def get_db():
-  """ Opens a new database connection if
-      there isn't one already.
-  """
-  if not hasattr(g, 'db'):
-    g.db = connect_db()
-  return g.db
-
-def get_cursor():
-  """ Returns a cursor to the current
-      database connection
-  """
-  db = get_db()
-  return db.cursor()
-
-@app.teardown_appcontext
-def close_db(exception):
-  """ Close the database connection. """
-  if hasattr(g, 'db'):
-    g.db.close()
+    """ Returns a new database interface if
+        there isn't one already.
+    """
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db
 
 @app.route("/")
 def index():
@@ -65,130 +53,98 @@ def index():
 
 @app.route("/about")
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
 @app.route("/contact")
 def contact():
-  return render_template('contact.html')
-
-def get_n_bicycle():
-  cur = get_cursor()
-  cur.execute("SELECT COUNT(*) FROM bicycle_parking")
-  return int(cur.fetchone()[0])
-
-def get_max_rate():
-  cur = get_cursor()
-  cur.execute("SELECT MAX(rate_norm) FROM bicycle_rates")
-  return float(cut.fetchone()[0])
+    return render_template('contact.html')
 
 def prettify_str(name):
-  # Strip leading 0s from street addresses (e.g. 04th St)
-  name = re.sub(r'(\s|^)0+', r' ', name)
-  # Title case it
-  name = name.title()
-  # Fix title casing for ordinal numbers (e.g. 4Th)
-  name = re.sub(r'[0-9][SNRT]', lambda m: m.group(0).lower(), name)
-  # Fix title casing for possessive
-  name = re.sub(r'\'S(\s|$)', lambda m: m.group(0).lower(), name)
+    """ Makes street addresses look good """
+    # Strip leading 0s from street addresses (e.g. 04th St)
+    name = re.sub(r'(\s|^)0+', r' ', name)
+    # Title case it
+    name = name.title()
+    # Fix title casing for ordinal numbers (e.g. 4Th)
+    name = re.sub(r'[0-9][SNRT]', lambda m: m.group(0).lower(), name)
+    # Fix title casing for possessive
+    name = re.sub(r'\'S(\s|$)', lambda m: m.group(0).lower(), name)
 
-  return name
+    return name
 
 def pick_name_address(name, address):
-  """ Picks a name, address combination that makes sense
-      given that either can be missing """
-  if address == 'None':
-    address = ""
-  if 'undetermined' in name.lower():
-    name = address
-    address = ""
-  return (name, address)
+    """ Picks a name, address combination that makes sense
+        given that either can be missing """
+    if address == 'None':
+      address = ""
+    if 'undetermined' in name.lower():
+      name = address
+      address = ""
+    return (name, address)
 
-def get_nearby_bicycle(point, max_d):
-  n_bicycle = get_n_bicycle()
-
-  location = ("""ST_Transform(
-                  ST_GeomFromText(
-                      'POINT(%f %f)', 4326), 26943)"""
-              %(point['lon'], point['lat']))
-  query = ("""SELECT bicycle_parking.id,
-                      ST_Distance(%s, location),
-                      lon,
-                      lat,
-                      location_name,
-                      address,
-                      rate_norm,
-                      rank
-               FROM bicycle_parking INNER JOIN
-                 (SELECT id, rate_norm, rank() OVER (ORDER BY rate_norm DESC)
-                  FROM bicycle_rates) AS rates
-               ON bicycle_parking.id = rates.id
-               WHERE ST_DWithin(%s, location, %%s)
-               ORDER BY ST_Distance(%s, location)"""
-               %(location, location, location))
-  cur = get_cursor()
-  cur.execute(query, (max_d,))
-  markers = []
-  for row in cur:
-    marker = {}
-    marker['id'] = int(row[0])
-    marker['distance'] = float(row[1])
-    marker['longitude'] = float(row[2])
-    marker['latitude'] = float(row[3])
-    name = prettify_str(row[4])
-    address = prettify_str(row[5])
-    (marker['name'],
-     marker['address']) = pick_name_address(name, address)
-    marker['rate'] = row[6]
-    marker['score'] = 100*row[7]/n_bicycle
-    marker['note'] = ""
-    markers.append(marker)
-  return markers
+def format_parking_for_output(parking):
+    """ Formats the data so that it looks good when shown
+        to the end user """
+    for spot in parking:
+        spot['location_name'] = prettify_str(spot['location_name'])
+        spot['address'] = prettify_str(spot['address'])
+        (spot['location_name'],
+         spot['address']) = pick_name_address(spot['location_name'], spot['address'])
+        spot['score'] = "%0.0f"%(spot['score'])
+        # 1 mile = 1609.344 m
+        spot['distance'] = "%0.2f mi"%(spot['distance']/1609.344)
+    return parking
 
 @app.route("/search", methods=['GET'])
 def search():
-  try:
-    point = {"lat": float(request.args['lat']),
-             "lon": float(request.args['lon'])}
-    max_d = float(request.args['max_d'])
-  except ValueError:
-    return jsonify(status = 'FAIL',
-                   description = 'Could not parse arguments.')
+    try:
+        point = {"lat": float(request.args['lat']),
+                 "lon": float(request.args['lon'])}
+        max_d = float(request.args['max_d'])
+    except ValueError:
+        return jsonify(status = 'FAIL',
+                       center = point,
+                       description = 'Could not parse arguments.')
 
-  markers = get_nearby_bicycle(point, max_d)
-  for marker in markers:
-    if '444' in marker['address']:
-      print marker
-  if len(markers) > 0:
-    safest = max(markers, key=itemgetter('score'))
-    closest = min(markers, key=itemgetter('distance'))
-    safest['note'] = "safest"
-    closest['note'] = "closest"
-    if (safest == closest):
-      safest['note'] = 'safest and closest'
+    parking = get_db().get_nearby_parking('bicycle', point, max_d)
+    max_rate = max((x['rate'] for x in parking))
+    for spot in parking:
+        spot['parksafescore'] = (spot['distance']/250)**2 + (spot['rate']/1.1)**2
+    if len(parking) > 0:
+        safest = max(parking, key=itemgetter('score'))
+        closest = min(parking, key=itemgetter('distance'))
+        safest['safest'] = True
+        closest['closest'] = True
 
-    markers.sort(key = lambda x: (x['rate']/5)**2 + (x['distance']/300)**2)
-    suggested = markers[:5]
-  else:
-    suggested = []
-  return jsonify(n = len(suggested), center = point,
-                 status = "OK",
-                 addresses = [x['address'] for x in suggested],
-                 names = [x['name'] for x in suggested],
-                 scores = ["%0.0f"%(x['score']) for x in suggested],
-                 distances = ["%0.2f mi"%(x['distance']/1609.344) for x in suggested],
-                 latitudes = [x['latitude'] for x in suggested],
-                 longitudes = [x['longitude'] for x in suggested],
-                 notes = [x['note'] for x in suggested]
-                 )
+        suggested = [safest]
+        parking.remove(safest)
+        if safest != closest:
+            suggested.append(closest)
+            parking.remove(closest)
+
+        # Basically we want a list of results that includes the closest,
+        # the safest, and the remaning top-ranked ones, sorted in order
+        # of their rank
+        # Reverse so pop() gets the lowest saferank (which is better)
+        parking.sort(key = itemgetter('parksafescore'), reverse=True)
+        while len(parking) > 0 and len(suggested) < app.config['MAX_RESULTS']:
+            suggested.append(parking.pop())
+        suggested.sort(key = itemgetter('parksafescore'))
+
+        suggested = format_parking_for_output(suggested)
+        return jsonify(status = 'OK',
+                       center = point,
+                       results = suggested,
+                       n = len(suggested))
+    else:
+        return jsonify(status = 'NONE_FOUND',
+                       center = point,
+                       n = 0,
+                       description = "Found no results for location and radius.")
 
 @app.route("/map", methods=['GET'])
 def map():
-  return render_template("map.html")
+    return render_template("map.html")
 
 if __name__ == "__main__":
-  with app.test_request_context():
-    print url_for('index')
-    print url_for('search')
-    print url_for('map')
-
-  app.run()
+    app.run()
