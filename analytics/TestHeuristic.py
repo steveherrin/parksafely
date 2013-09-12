52d601f6957601c68ed56b89d76eb15d2cca8cbc
Steve Herrin <steve.herrin@gmail.com>
2013-09-12 11:54:39 -0700

    Actually record the test statistic and not the validation statistic.

--- analytics/TestHeuristic.py
+++ analytics/TestHeuristic.py
@@ -42,62 +42,62 @@ cur = db.cursor
 cur.execute("""SELECT incident_num,
                       ST_X(ST_Transform(location, 4326)) AS lon,
                       ST_Y(ST_Transform(location, 4326)) AS lat,
-                      at_police_station
+                      CAST(EXTRACT(DOY FROM t) AS int) %2
                FROM crimes
                WHERE vehicle='bicycle'
-               AND EXTRACT(YEAR FROM t) >= 2013
-               AND CAST(EXTRACT(DOY FROM t) AS int) % 2 = 1 """)
+               AND NOT at_police_station
+               AND EXTRACT(YEAR FROM t) >= 2013 """)
 
 max_d = float(402.336)
 
 r_w = float(sys.argv[1])
 
-extra_ds = []
-risk_factors = []
-n_zeros = 0
+# Consider odd and even number days.
+# Odd is used for testing and reported here.
+# Even is the estimate of performance and goes into DB.
+extra_ds = [[],[]]
+risk_factors = [[],[]]
+n_zeros = [0, 0]
 
 for crime in cur:
-
     point = { 'lon' : float(crime[1]),
               'lat' : float(crime[2])}
     incident_num = crime[0]
-    if crime[3]:
-        # Ignore suff reported at the Hall of Justice
-        # Since that really could be anywhere
-        continue
+    i = crime[3]
 
     parking = db.get_nearby_parking('bicycle', point, max_d)
     if len(parking) == 0:
         continue
     for spot in parking:
-        spot['metric'] = abs(spot['distance']/250)**2 + abs(spot['rate']/r_w)**2
+        spot['metric'] = abs(spot['distance']/250) + abs(spot['rate']/r_w)
 
     best = min(parking, key=itemgetter('metric'))
     closest = min(parking, key=itemgetter('distance'))
 
-    extra_ds.append(best['distance'] - closest['distance'])
+    extra_ds[i].append(best['distance'] - closest['distance'])
 
     rate_there = get_location_rate(db.cursor, point)
     #print best['rate'], best['distance'], rate_there
 
     if closest['rate'] == 0:
-        n_zeros +=1
+        n_zeros[i] +=1
         continue
     else:
-        risk_factors.append(best['rate']/closest['rate'])
+        risk_factors[i].append(best['rate']/closest['rate'])
 
-extra_ds.sort(reverse = True)
-risk_factors.sort(reverse = True)
+for i in xrange(2):
+    extra_ds[i].sort(reverse = True)
+    risk_factors[i].sort(reverse = True)
 
 print "Using top pick:"
-print "  walk %0.3f more on average"%(np.mean(extra_ds))
-print "  risk is muliplied by %0.3e on average"%(np.mean(risk_factors))
-print "  There were %i zeros."%(n_zeros)
+print "  walk %0.3f more on average"%(np.mean(extra_ds[1]))
+print "  risk is muliplied by %0.3e on average"%(np.mean(risk_factors[1]))
+print "  There were %i zeros."%(n_zeros[1])
 
 db.cursor.execute(""" INSERT INTO recommendation_stats (rate_scale,
                                                      avg_extra_distance,
                                                      risk_ratio)
                       VALUES (%s, %s, %s) """,
-                      (r_w, np.mean(extra_ds), np.mean(risk_factors)))
+                      (r_w, np.mean(extra_ds[0]), np.mean(risk_factors[0])))
 db.connection.commit()
 
